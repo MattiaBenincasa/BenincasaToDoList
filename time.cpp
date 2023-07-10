4aa6b0f0171f64b064def2beb490954f2a64a062
MattiaBenincasa <mattia.benincasa03@gmail.com>
2023-07-10 22:06:24 +0200

    Setting up cmake of the project and unit testing

---
+++ time.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mattia Benincasa on 10/07/23.
+//
+
+#include "time.h"
---
+++ time.h
@@ -0,0 +1,8 @@
+//
+// Created by Mattia Benincasa on 10/07/23.
+//
+
+#ifndef BENINCASATODOLIST_TIME_H
+#define BENINCASATODOLIST_TIME_H
+
+#endif //BENINCASATODOLIST_TIME_H
