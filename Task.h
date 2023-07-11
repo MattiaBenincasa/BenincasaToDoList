a742b0959e0d695e0de65d85dacd5a1b0de572e6
MattiaBenincasa <mattia.benincasa03@gmail.com>
2023-07-11 16:55:18 +0200

    Created class Task

--- Task.h
+++ Task.h
@@ -4,5 +4,32 @@
 
 #ifndef BENINCASATODOLIST_TASK_H
 #define BENINCASATODOLIST_TASK_H
+#include <string>
+#include "time.h"
+#include "Date.h"
+
+class Task {
+public:
+    Task(std::string& n, std::string& desc, Date d, Time t) : name(n), description(desc), day(d), time(t){}
+
+    std::string getName() {
+        return name;
+    }
+
+    void isCompleted() {
+        completed = true;
+    }
+
+    void notCompleted() {
+        completed = false;
+    }
+
+private:
+    std::string name;
+    std::string description;
+    Date day;
+    Time time;
+    bool completed {false};
+};
 
 #endif //BENINCASATODOLIST_TASK_H
