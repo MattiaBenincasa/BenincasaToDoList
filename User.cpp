7ccd1ccc463d67b8ad8d388221622f63035e0755
MattiaBenincasa <mattia.benincasa03@gmail.com>
2023-07-11 21:54:00 +0200

    Definition of User class methods.

--- User.cpp
+++ User.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "User.h"
+
+void User::addTask(std::string &n, std::string &desc, Date d, Clock t) {
+    Task task(n, desc, d, t);
+    tasks.insert(std::make_pair(task.getName(), task));
+}
+
+void User::removeTask(std::string &n) {
+    auto findTask = tasks.find(n);
+    tasks.erase(findTask);
+}
+
+void User::markCompleted(std::string &n) {
+    auto findTask = tasks.find(n);
+    findTask->second.isCompleted();
+}
+
+void User::markNotCompleted(std::string &n) {
+    auto findTask = tasks.find(n);
+    findTask->second.notCompleted();
+}
